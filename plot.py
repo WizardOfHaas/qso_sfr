050eae2e0e9bc579113d4b8eebb6d5a6dbd831be
Sean Haas <wizardofhaas1024@gmail.com>
2017-03-09 23:29:33 -0800

    .

--- plot.py
+++ plot.py
@@ -21,7 +21,7 @@ def sfr(file):
 	
 		z = float(data[5].rstrip())
 		f_U = float(data[3]) * (10 ** -9)
-
+		
 		if f_U > 0 and z > 0:
 			d = ((z * c) / H0) / pc
 
@@ -29,12 +29,16 @@ def sfr(file):
 			M_U = 2.5 * math.log(d, 10) + 5 + m_U
 			L_U = 10 ** ((M_sun - M_U)/2.5) * L_sun
 
+			#F = L/(4 pi D^2)
+			#mAB = -2.5 Log(F) - 48.6
+
 			flux.append([
 				L_U / 5.9e21,
 				z
 			])
 
 	num_samples = len(flux) * 1.0
+	flux = np.array(flux)
 
 	#Bin data by z
 	sfr_mean = []
@@ -45,7 +49,7 @@ def sfr(file):
 		sfr_mean.append(
 			[
 				math.log(np.mean(z_bin[:,0]), 10) if len(z_bin) > 0 else 0,
-				z,
+				z + dz / 2.,
 				len(z_bin) / num_samples
 			]
 		)
@@ -53,13 +57,13 @@ def sfr(file):
 
 	#Bin data by SFR
 	sfr_bin = []
-	dsfr = 0.5
-	for s in np.arange(0, 3.5, dsfr):
+	dsfr = 0.5e-13
+	for s in np.arange(0, 0.5e-12, dsfr):
 		s_bin = np.array(filter(lambda x: x[0] > s and x[0] <= s + dsfr, flux))
 
 		sfr_bin.append(
 			[
-				s,
+				s + dsfr / 2.,
 				len(s_bin) / num_samples
 			]
 		)
@@ -76,29 +80,29 @@ gal_sfr_z, gal_sfr_bin = sfr('gal_flux.csv')
 
 plt.figure(1)
 
-plt.subplot(311)
+plt.subplot(221)
 plt.plot(qso_sfr_z[:,1], qso_sfr_z[:,0], 'ro', label='QSO')
 plt.plot(gal_sfr_z[:,1], gal_sfr_z[:,0], 'bo', label='Galaxy')
 
 plt.title("U Band Derived SFR Histories: SDSS DR12")
-plt.legend(loc='upper left')
+plt.legend(loc='upper right')
 plt.xlabel("z")
-plt.ylabel("SFR (log(M sun yr^-1))")
+plt.ylabel("SFR [log(M sun yr^-1)]")
 
-plt.subplot(312)
+plt.subplot(222)
 plt.plot(qso_sfr_z[:,1], qso_sfr_z[:,2], 'ro', label='QSO')
 plt.plot(gal_sfr_z[:,1], gal_sfr_z[:,2], 'bo', label='Galaxy')
 
 plt.title("% Population at Given Z: SDSS DR12")
 plt.xlabel("z")
-plt.ylabel("Pop (%)")
+plt.ylabel("Pop [%]")
 
-plt.subplot(313)
+plt.subplot(223)
 plt.plot(qso_sfr_bin[:,0], qso_sfr_bin[:,1], 'ro', label='QSO')
 plt.plot(gal_sfr_bin[:,0], gal_sfr_bin[:,1], 'bo', label='Galaxy')
 
 plt.title("% Population with Given SFR: SDSS DR12")
-plt.xlabel("SFR (M sun yr^-1)")
-plt.ylabel("Pop (%)")
+plt.xlabel("SFR [M sun yr^-1]")
+plt.ylabel("Pop [%]")
 
 plt.show()
